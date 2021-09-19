6552e133f3de9496033a41a71a6c2f6578b5552e
Syntax Error <63340001+Alpha-404@users.noreply.github.com>
2021-09-19 08:12:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-var allImages = document.images;
-var numImages = 0
-for(var i=0; i<allImages.length; i++) {
-	replaceImage(allImages[i]);
-}
-function replaceImage(image){
-	image.setAttribute("style", `height:${image.height}px; width:${image.width}px; object-fit:cover; content:url(${"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2SihDg2em30I0fS8lZ1jsQHaE8%26pid%3DApi&f=1"});`);
-	image.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2SihDg2em30I0fS8lZ1jsQHaE8%26pid%3DApi&f=1";
+for(var i=0; i<document.images.length; i++) {
+	document.images[i].setAttribute("style", `height:${document.images[i].height}px; width:${document.images[i].width}px; object-fit:cover; content:url(${"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2SihDg2em30I0fS8lZ1jsQHaE8%26pid%3DApi&f=1"});`);
+	document.images[i].src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2SihDg2em30I0fS8lZ1jsQHaE8%26pid%3DApi&f=1";
 }
